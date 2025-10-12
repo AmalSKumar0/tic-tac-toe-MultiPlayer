6faf1046d11c930b7252eafde6b0b4ac844f62c8
Amal s kumar <amalskumarofficialz@gmail.com>
2025-10-12 07:47:54 +0530

    Commit for deployment

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -1,11 +1,19 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# This line must come FIRST, before any other Django imports.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+
+# This call is necessary to initialize Django's settings and app registry.
+django.setup()
+
+# Now that Django is configured, you can safely import your other modules.
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.middleware import JWTAuthMiddleware
 import api.routing 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
-
+# Your application definition
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": JWTAuthMiddleware(
